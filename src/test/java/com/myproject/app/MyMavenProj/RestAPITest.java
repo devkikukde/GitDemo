2b857e9a7d85a9a24dbde1f801cbbdc24d0f0e0b
devki <devkikukde2610@gmail.com>
2019-07-25 13:58:16 -0400

    First commit

---
+++ src/test/java/com/myproject/app/MyMavenProj/RestAPITest.java
@@ -0,0 +1,18 @@
+package com.myproject.app.MyMavenProj;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void deleteUserInfo() {
+		System.out.println("deleteUserInfo");
+	}
+	
+	@Test
+	public void addUserInfo() {
+		System.out.println("addUserInfo");
+	}
+
+
+}
---
+++ src/test/java/com/myproject/app/MyMavenProj/SeleniumTest.java
@@ -0,0 +1,17 @@
+package com.myproject.app.MyMavenProj;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserAutomation() {
+		System.out.println("browserAutomation");
+	}
+	
+	@Test
+	public void testUIElements() {
+		System.out.println("testUIElements");
+	}
+
+}
